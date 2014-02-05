59b77a8333958b961d6567b2123d8ae5467b2793
Eskimon <eskimon@outlook.com>
2014-02-06 00:22:41 +0100

    Début d'implémentation de front-end de test

--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,62 @@
 "use strict";
 
+function getCourses() {
+  var request = new XMLHttpRequest();
+  request.open("GET", "/api/courses/", true);
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4){
+      if (this.status >= 200 && this.status < 400){
+        // Success!        
+        var ret = JSON.parse(this.responseText);
+        var list = document.getElementById("listCourses");
+        var content = "";
+        for (var i = 0; i < ret.length; i++) {
+          content += "<li data-id=\""+ ret[i]._id +"\" class=\"courses\">"+ ret[i].name + "</li>";
+        }
+        list.innerHTML = content;
+      } else {
+        // Error :(
+      }
+    }
+  };
+
+  var data = {"Authorization" : "Basic cm9vdDphbHBpbmU="};
+
+  request.send(data);
+  request = null;
+}
+
+function getStudents() {
+  var request = new XMLHttpRequest();
+  request.open("GET", "/api/students/", true);
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4){
+      if (this.status >= 200 && this.status < 400){
+        // Success!        
+        var ret = JSON.parse(this.responseText);
+        var list = document.getElementById("listStudents");
+        var content = "";
+        for (var i = 0; i < ret.length; i++) {
+          content += "<li data-id=\""+ ret[i]._id +"\" class=\"students\">" + ret[i].name.first + " " + ret[i].name.last + "</li>";
+        }
+        list.innerHTML = content;
+      } else {
+        // Error :(
+      }
+    }
+  };
+
+  var data = {"Authorization" : "Basic cm9vdDphbHBpbmU="};
+
+  request.send(data);
+  request = null;
+}
+
+
 (function() {
-  console.log("Some JS code");
+	getCourses();
+	getStudents();
 })();
+
