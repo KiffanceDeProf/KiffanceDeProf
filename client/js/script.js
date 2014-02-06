cf071cc9c52dcf337a019cc2ef655127c45e44fb
Quentin Gliech <quentingliech@gmail.com>
2014-02-06 20:15:39 +0100

    Intégration des templates dans l'HTML + Clean du JS admin

--- client/js/script.js
+++ client/js/script.js
@@ -3,30 +3,29 @@
 var Mustache = window.Mustache;
 
 var templates = {
-  students: "{{#students}}<tr data-id=\"{{_id}}\"> <td>{{_id}}</td> <td>{{name.last}}, {{name.first}}</td> <td>{{description}}</td> <td>{{course}}</td> </tr> {{/students}}",
-  courses: "{{#courses}}<tr data-id=\"{{_id}}\"> <td>{{_id}}</td> <td>{{name}},</td> <td>{{description}}</td> <td>{{type}}ème</td> </tr> {{/courses}}"
+  students: document.getElementById("templateStudents").innerHTML,
+  courses: document.getElementById("templateCourses").innerHTML
 };
+
+var authHeader = "Basic cm9vdDphbHBpbmU=";
 
 function getCourses() {
   var request = new XMLHttpRequest();
   request.open("GET", "/api/courses/", true);
 
-  request.onreadystatechange = function() {
-    if (this.readyState === 4){
-      if (this.status >= 200 && this.status < 400){
-        // Success!        
-        var ret = JSON.parse(this.responseText);
-        var list = document.getElementById("listCourses");
-        list.innerHTML = Mustache.to_html(templates.courses, { courses: ret });
-      } else {
-        // Error :(
-      }
+  request.onload = function() {
+    if (this.status === 200){
+      // Success!        
+      var ret = JSON.parse(this.responseText);
+      var list = document.getElementById("listCourses");
+      list.innerHTML = Mustache.to_html(templates.courses, { courses: ret });
+    } else {
+      // Error :(
     }
   };
 
-  var data = {"Authorization" : "Basic cm9vdDphbHBpbmU="};
-
-  request.send(data);
+  request.setRequestHeader("Authorization", authHeader);
+  request.send();
   request = null;
 }
 
@@ -34,22 +33,19 @@ function getStudents() {
   var request = new XMLHttpRequest();
   request.open("GET", "/api/students/", true);
 
-  request.onreadystatechange = function() {
-    if (this.readyState === 4){
-      if (this.status >= 200 && this.status < 400){
-        // Success!        
-        var ret = JSON.parse(this.responseText);
-        var list = document.getElementById("listStudents");
-        list.innerHTML = Mustache.to_html(templates.students, { students: ret });
-      } else {
-        // Error :(
-      }
+  request.onload = function() {
+    if (this.status === 200){
+      // Success!
+      var ret = JSON.parse(this.responseText);
+      var list = document.getElementById("listStudents");
+      list.innerHTML = Mustache.to_html(templates.students, { students: ret });
+    } else {
+      // Error :(
     }
   };
 
-  var data = {"Authorization" : "Basic cm9vdDphbHBpbmU="};
-
-  request.send(data);
+  request.setRequestHeader("Authorization", authHeader);
+  request.send();
   request = null;
 }
 
