d0b0e3eba98e68e6464130186db4cba4b9e18296
Quentin Gliech <quentingliech@gmail.com>
2014-04-22 13:51:58 +0200

    Migration from express 3 to 4

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var express = require("express"),
     app = express(),
     mongoose = require("mongoose"),
     passport = require("passport"),
+    bodyParser = require("body-parser"),
+    cookieParser = require("cookie-parser"),
+    compress = require("compression")(),
+    session = require("express-session"),
+    serveStatic = require("serve-static"),
+    morgan = require("morgan"),
     models = require("./app/models")(mongoose),
     routes = require("./app/routes");
 
@@ -24,26 +30,22 @@ db.on("open", function() {
   console.log("✔ Connected to MongoDB");
 });
 
-app.configure(function() {
-  app.set("models", models);
-  app.set("mongoose", mongoose);
-  app.set("api path", "/api");
-  app.set("passport", passport);
+app.set("models", models);
+app.set("mongoose", mongoose);
+app.set("api path", "/api");
+app.set("passport", passport);
 
-  app.use(express.logger("short"));
-  app.use(express.compress());
-  app.use(express.cookieParser("yolo"));
-  app.use(express.session());
-  app.use(passport.initialize());
-  app.use(express.bodyParser());
-  app.use(express.json());
-  app.use(express.urlencoded());
-  app.use(app.router);
-  app.use(express.static(__dirname + "/public")); // Pour le contenu static
-});
+app.use(morgan("short"));
+app.use(compress);
+app.use(cookieParser("yolo"));
+app.use(session());
+app.use(passport.initialize());
+app.use(bodyParser());
 
 require("./app/passport").setup(passport, mongoose);
 routes.setup(app);
+
+app.use(serveStatic(__dirname + "/public")); // Pour le contenu static
 
 app.listen(1337 || process.env.PORT, function() {
   console.log("✔ Server running on port", 1337 || process.env.PORT);
